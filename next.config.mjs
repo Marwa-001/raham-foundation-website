f57ed3509cdfbb36c3b2e001d16637b3c0d11fc4
Marwa-001 <marwaamjad6578@gmail.com>
2026-08-04 00:20:49 +0500

    chore: update package.json and add wrangler configuration
    - Added new scripts for Cloudflare bundling, previewing, and deploying in package.json.
    - Included @opennextjs/cloudflare as a dependency.
    - Added wrangler as a devDependency.
    - Created wrangler.jsonc for Cloudflare Worker configuration, including asset and image optimization settings.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "standalone",
   images: {
     unoptimized: true,
   },
 };
+
+if (process.env.NODE_ENV !== "production") {
+  import("@opennextjs/cloudflare").then(({ initOpenNextCloudflareForDev }) => {
+    initOpenNextCloudflareForDev();
+  });
+}
 
 export default nextConfig;
